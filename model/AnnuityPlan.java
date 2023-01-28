a4912b1ed639c7f5b4e435300d0a2cee212c64b8
Phillip Kim <phillipkakg@gmail.com>
2023-01-28 02:57:02 -0500

    wrote out generate cashflows method

--- model/AnnuityPlan.java
+++ model/AnnuityPlan.java
@@ -1,19 +1,54 @@
+import java.lang.Math;
+
 public class AnnuityPlan {
     private double withdrawal;
     private double duration;
+    private int compoundFrequency;
+    private double interest;
 
-    public AnnuityPlan(double withdrawal, double duration) {
+    public AnnuityPlan(double withdrawal, int duration, int compoundFrequency, double interest) {
         this.withdrawal = withdrawal;
         this.duration = duration;
+        this.compoundFrequency = compoundFrequency;
+        this.interest = interest;
     }
 
     public double returnWithdrawal() {
         return this.withdrawal;
     }
 
+    public double returnDuration() {
+        return this.duration;
+    }
+
+    public int returnCompoundFrequency() {
+        return this.compoundFrequency;
+    }
+
+    public double returnInterest() {
+        return this.interest;
+    }
+
+    public static Double[] getCashFlows(double withdrawal, int duration, int compoundFrequency, double interest) {
+        int totalPeriods = duration * compoundFrequency;
+        Double[] cashFlows = new Double[totalPeriods];
+
+        for (int i = 0; i < totalPeriods; i++) {
+            cashFlows[i] = withdrawal * Math.pow(1 + interest, i);
+        }
+
+        return cashFlows;
+    }
+
     public static void main(String... arg) {
-        AnnuityPlan obj = new AnnuityPlan(3000, 20);
+        AnnuityPlan obj = new AnnuityPlan(3000, 20, 4, 0.04);
         System.out.println(obj.returnWithdrawal());
+        Double[] cashFlows = new Double[80];
+        cashFlows = AnnuityPlan.getCashFlows(3000, 20, 4, 0.04);
+
+        for (int i = 0; i < cashFlows.length; i++) {
+            System.out.println(cashFlows[i]);
+        }
     }
 
 }
