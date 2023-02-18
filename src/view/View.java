fb4be80aceadc4f5dfe11557be2cd0ee1216dad4
Phillip Kim <phillipkakg@gmail.com>
2023-02-18 01:42:36 -0500

    renamed jframe class to be view instead

--- src/view/View.java
+++ src/view/View.java
@@ -2,6 +2,30 @@ package src.view;
 
 import javax.swing.*;
 
-public class View {
+import src.view.panels.AnnuityPlanConfigPanel;
+import src.view.panels.AnnuityPlanPanel;
+import src.view.panels.SubmissionPanel;
 
+public class View extends JFrame {
+    public AnnuityPlanConfigPanel annuityPlanConfigPanel = new AnnuityPlanConfigPanel();
+    public AnnuityPlanPanel annuityPlanPanel = new AnnuityPlanPanel();
+    public SubmissionPanel submissionPanel = new SubmissionPanel();
+
+    public MainPanel mainPanel = new MainPanel();
+
+    public View() {
+        setTitle("JJEFA");
+        setBounds(100,100,800,800);
+
+        mainPanel.add(annuityPlanConfigPanel);
+        mainPanel.add(annuityPlanPanel);
+        mainPanel.add(submissionPanel);
+
+        add(mainPanel);
+    }
+
+    public static void main(String... arg) {
+        View annuityPlanFrame = new View();
+        annuityPlanFrame.setVisible(true);
+    }
 }
